5067da54af7e91c71fdefa4d14d5325347e4acf7
Michael Schout <schoutm@gmail.com>
2026-02-20 14:44:09 -0600

    Build results of e9f868e (on master)

--- lib/Dist/Zilla/Plugin/AuthorSignatureTest.pm
+++ lib/Dist/Zilla/Plugin/AuthorSignatureTest.pm
@@ -43,7 +43,7 @@ sub munge_file {
     warn "Processing file: ", $file->name;
 
     return unless $file->name eq 'xt/author/signature.t'
-     || $file->name eq 't/author/signature.t';
+     || $file->name eq 't/author-signature.t';
 
     my %vars = (
         force => $self->force ? q{force_} : q{},
